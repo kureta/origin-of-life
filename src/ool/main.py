a3ad0dc927bbdc43082448211a585932f3279932
kureta <skureta@gmail.com>
2024-08-30 13:47:17 +0300

    Using standard pytorch tensor shapes
    batch, ch, w, h for state and in_ch, out_ch, w, h for kernel

--- src/ool/main.py
+++ src/ool/main.py
@@ -8,16 +8,20 @@ torch.autograd.set_grad_enabled(False)
 
 def torus_pad(input_tensor, pad_width):
     padded_tensor = torch.cat(
-        [input_tensor[:, -pad_width:, :], input_tensor, input_tensor[:, :pad_width, :]],
-        dim=1,
+        [
+            input_tensor[..., -pad_width:, :],
+            input_tensor,
+            input_tensor[..., :pad_width, :],
+        ],
+        dim=-2,
     )
     padded_tensor = torch.cat(
         [
-            padded_tensor[-pad_width:, :, :],
+            padded_tensor[..., -pad_width:],
             padded_tensor,
-            padded_tensor[:pad_width, :, :],
+            padded_tensor[..., :pad_width],
         ],
-        dim=0,
+        dim=-1,
     )
 
     return padded_tensor
@@ -27,10 +31,10 @@ def rule(state, neighbor_count):
     return (neighbor_count == 3) | ((state == 1) & (neighbor_count == 2))
 
 
-# TODO: kernel has dimensions batch, in_channels, out_channels, height, width
-# TODO: state has dimensions batch, in_channels, height, width
+# NOTE: kernel has dimensions in_channels, out_channels, height, width
+# NOTE: state has dimensions batch, in_channels, height, width
 # TODO: instead of using torus_pad treat state size as one larger than the actual size
-#       and identify edges with inner edges
+#       and identify edges with opposite inner edges
 # TODO: either modify `state` property in-place or return new state. Do not mix both.
 
 
@@ -38,27 +42,23 @@ class App:
     def __init__(self) -> None:
         pr.set_trace_log_level(pr.TraceLogLevel.LOG_WARNING)
         pr.init_window(800, 450, "Hello")
-        self.kernel = torch.ones(3, 3, dtype=torch.uint8)
-        self.kernel[1, 1] = 0
-        self.state = torch.randint(0, 2, (100, 100, 1), dtype=torch.uint8)
+        self.kernel = torch.ones(1, 1, 3, 3, dtype=torch.uint8)
+        self.kernel[..., 1, 1] = 0
+        self.initialize_state()
+
+    def initialize_state(self):
+        self.state = torch.randint(0, 2, (1, 1, 100, 100), dtype=torch.uint8)
 
     def count_neighbors(self):
         state = torus_pad(self.state, 1)
-        return (
-            F.conv2d(
-                state.permute(2, 0, 1).unsqueeze(0),
-                self.kernel.unsqueeze(0).unsqueeze(0),
-            )
-            .squeeze(0)
-            .permute(1, 2, 0)
-        )
+        return F.conv2d(state, self.kernel)
 
     def update_state(self):
         neighbor_count = self.count_neighbors()
         self.state = rule(self.state, neighbor_count).to(torch.uint8)
 
     def update_texture(self):
-        img_data = (self.state * 255).numpy()
+        img_data = (self.state.squeeze() * 255).numpy()
         img = pr.Image(
             img_data,
             100,
@@ -79,12 +79,11 @@ class App:
         pr.draw_texture(self.tex, 0, 0, pr.WHITE)
         pr.draw_fps(190, 200)
         pr.end_drawing()
-        # sleep(1)
 
     def start(self):
         while not pr.window_should_close():
             if pr.is_key_released(pr.KeyboardKey.KEY_SPACE):
-                self.state = torch.randint(0, 2, (100, 100, 1), dtype=torch.uint8)
+                self.initialize_state()
             self.loop()
         pr.close_window()
 
