c099e862fd783e10bbe354e7daf9a14705fbb6e8
kureta <skureta@gmail.com>
2024-08-30 17:02:03 +0300

    Processing in pairs

--- src/ool/main.py
+++ src/ool/main.py
@@ -27,6 +27,31 @@ def torus_pad(input_tensor, pad_width):
     return padded_tensor
 
 
+# swaps outer edges of 2 channels of a tensor
+def swap_edges(state):
+    (
+        state[:, 0, 0, :],
+        state[:, 1, 0, :],
+        state[:, 0, -1, :],
+        state[:, 1, -1, :],
+        state[:, 0, :, 0],
+        state[:, 1, :, 0],
+        state[:, 0, :, -1],
+        state[:, 1, :, -1],
+    ) = (
+        state[:, 1, 0, :],
+        state[:, 0, 0, :],
+        state[:, 1, -1, :],
+        state[:, 0, -1, :],
+        state[:, 1, :, 0],
+        state[:, 0, :, 0],
+        state[:, 1, :, -1],
+        state[:, 0, :, -1],
+    )
+
+    return state
+
+
 def rule(state, neighbor_count):
     return (neighbor_count == 3) | ((state == 1) & (neighbor_count == 2))
 
@@ -34,7 +59,6 @@ def rule(state, neighbor_count):
 # NOTE: kernel has dimensions out_channels, in_channels, height, width
 # NOTE: state has dimensions batch, in_channels, height, width
 # TODO: either modify `state` property in-place or return new state. Do not mix both.
-# TODO: when simulating life, take pairs, pad, and swap borders of the pairs.
 
 # TODO: list of adjustable parameters:
 #       - grid size
@@ -48,35 +72,38 @@ class App:
     def __init__(self) -> None:
         pr.set_trace_log_level(pr.TraceLogLevel.LOG_WARNING)
         pr.init_window(800, 450, "Hello")
-        self.kernel = torch.ones(1, 1, 3, 3, dtype=torch.uint8)
+        self.kernel = torch.ones(2, 1, 3, 3, dtype=torch.uint8)
         self.kernel[..., 1, 1] = 0
         self.distribution = Bernoulli(ALIVE_PROB)
         self.state: torch.Tensor
         self.initialize_state()
+        self.tex = []
 
     def initialize_state(self):
-        self.state = self.distribution.sample(torch.Size([1, 1, SIZE, SIZE])).to(
+        self.state = self.distribution.sample(torch.Size([1, 2, SIZE, SIZE])).to(
             torch.uint8
         )
 
     def count_neighbors(self):
         state = torus_pad(self.state, 1)
-        return F.conv2d(state, self.kernel)
+        state = swap_edges(state)
+        return F.conv2d(state, self.kernel, groups=2)
 
     def update_state(self):
         neighbor_count = self.count_neighbors()
         self.state = rule(self.state, neighbor_count).to(torch.uint8)
 
     def update_texture(self):
-        img_data = (self.state.squeeze() * 255).numpy()
-        img = pr.Image(
-            img_data,
-            SIZE,
-            SIZE,
-            1,
-            pr.PixelFormat.PIXELFORMAT_UNCOMPRESSED_GRAYSCALE,
-        )
-        self.tex = pr.load_texture_from_image(img)
+        self.tex.clear()
+        for img_data in self.state.squeeze().numpy():
+            img = pr.Image(
+                img_data * 255,
+                SIZE,
+                SIZE,
+                1,
+                pr.PixelFormat.PIXELFORMAT_UNCOMPRESSED_GRAYSCALE,
+            )
+            self.tex.append(pr.load_texture_from_image(img))
 
     def update(self):
         self.update_state()
@@ -86,7 +113,8 @@ class App:
         self.update()
         pr.begin_drawing()
         pr.clear_background(pr.WHITE)
-        pr.draw_texture(self.tex, 0, 0, pr.WHITE)
+        for idx, tex in enumerate(self.tex):
+            pr.draw_texture(tex, SIZE * idx, 0, pr.WHITE)
         pr.draw_fps(190, 200)
         pr.end_drawing()
 
